1ceefa497b4ebf553b7606afd45155ed697e5247
Sumaiya215 <sumaiyacse15>
2025-02-07 21:08:22 -0800

    addede payment system

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { default: axios } = require('axios');
 require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000;
@@ -10,6 +11,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7vw6m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -35,84 +37,99 @@ async function run() {
     const paymentCollection = client.db('bistroDb').collection('payments')
 
     // jwt related api
-    app.post('/jwt', async(req,res) =>{
+    app.post('/jwt', async (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-      expiresIn: '5h'});
-      res.send({token})
+        expiresIn: '5h'
+      });
+      res.send({ token })
     })
 
     // middlewares
-    const verifyToken = (req,res,next) =>{
-      console.log( 'inside verify token', req.headers.authorization);
-      if(!req.headers.authorization) {
-        return res.status(401).send({message: 'unauthorized access'});
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
       }
-        const token = req.headers.authorization.split(' ')[1];
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) =>{
-          if(error){
-            return res.status(401).send({message: 'unauthorized access'})
-          }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+        if (error) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
 
-          req.decoded = decoded;
-          next();
-        })
-        
+        req.decoded = decoded;
+        next();
+      })
+
     }
 
     // use verify admin after verifyToken
-    const verifyAdmin = async(req, res, next) => {
+    const verifyAdmin = async (req, res, next) => {
       const email = req.decoded.email;
-      const query = { email: email};
+      const query = { email: email };
       const user = await userCollection.findOne(query);
       const isAdmin = user?.role === 'admin';
-      if(!isAdmin){
-        return res.status(403).send({message: 'forbidden access'})
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' })
       }
 
       next();
     }
 
-    app.get('/users/admin/:email', verifyToken, async(req,res) =>{
+    // useAdmin 
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
       const email = req.params.email;
-      if(email !== req.decoded.email){
-        return res.status(403).send({message: 'forbidden access'})
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
       }
 
-      const query = {email: email};
+      const query = { email: email };
       const user = await userCollection.findOne(query)
       let admin = false;
-      if(user){
+      if (user) {
         admin = user?.role == 'admin'
       }
-      res.send({admin});
+      res.send({ admin });
     })
 
+    // Store ID: bistr679e3defafc8c
+    // Store Password (API/Secret Key): bistr679e3defafc8c@ssl
+
+
+    // Merchant Panel URL: https://sandbox.sslcommerz.com/manage/ (Credential as you inputted in the time of registration)
+
+
+
+    // Store name: testbistrseuh
+    // Registered URL: www.bistroboss.com
+    // Session API to generate transaction: https://sandbox.sslcommerz.com/gwprocess/v3/api.php
+    // Validation API: https://sandbox.sslcommerz.com/validator/api/validationserverAPI.php?wsdl
+    // Validation API (Web Service) name: https://sandbox.sslcommerz.com/validator/api/validationserverAPI.php
 
 
     // user related apis
-    app.get('/users', verifyToken, verifyAdmin, async(req,res) =>{ 
+    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
       const result = await userCollection.find().toArray()
       // console.log(result);
       res.send(result)
     })
 
-    app.post('/users', async(req,res) =>{
+    app.post('/users', async (req, res) => {
       const user = req.body;
-      const query = {email: user.email}
+      const query = { email: user.email }
       const existingUser = await userCollection.findOne(query)
-      if(existingUser){
-        return res.send({message:'User already exists', insertedId: null})
+      if (existingUser) {
+        return res.send({ message: 'User already exists', insertedId: null })
       }
       const result = await userCollection.insertOne(user)
       res.send(result);
     })
 
-    app.patch('/users/admin/:id', verifyToken, verifyAdmin, async(req,res)=>{
+    app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)}
+      const filter = { _id: new ObjectId(id) }
       const updatedDoc = {
-        $set:{
+        $set: {
           role: 'admin'
         }
       }
@@ -120,18 +137,18 @@ async function run() {
       res.send(result)
     })
 
-    app.delete('/users/:id', verifyToken, verifyAdmin, async(req,res)=>{
+    app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const result = await userCollection.deleteOne(query)
       res.send(result);
     })
 
 
     //menu related api
-    app.get('/menu', async(req,res) =>{
-        const result = await menuCollection.find().toArray();
-        res.send(result);
+    app.get('/menu', async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
     })
 
     app.get('/menu/:id', async (req, res) => {
@@ -141,18 +158,18 @@ async function run() {
       res.send(result);
     })
 
-    app.post('/menu', verifyToken, verifyAdmin, async(req,res) =>{
+    app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
       const item = req.body;
       const result = await menuCollection.insertOne(item);
       res.send(result);
     })
 
-   
 
 
-    app.delete('/menu/:id', verifyToken, verifyAdmin, async(req,res)=>{
+
+    app.delete('/menu/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const result = await menuCollection.deleteOne(query)
       console.log(result);
       res.send(result);
@@ -176,34 +193,34 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/reviews', async(req,res) =>{
-        const result = await reviewsCollection.find().toArray();
-        res.send(result);
+    app.get('/reviews', async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
     })
 
     // carts collection
-    app.get('/carts', async(req,res) =>{
+    app.get('/carts', async (req, res) => {
       const email = req.query.email;
-      const query = {email: email};
+      const query = { email: email };
       const result = await cartCollection.find(query).toArray();
       res.send(result);
     })
 
-    app.post('/carts', async(req,res) =>{
+    app.post('/carts', async (req, res) => {
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
       res.send(result);
     })
 
-    app.delete('/carts/:id', async(req,res) =>{
+    app.delete('/carts/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
 
     // stripe related api
-    app.post('/create-payment-intent', async(req,res) =>{
+    app.post('/create-payment-intent', async (req, res) => {
       const { price } = req.body;
       const amount = parseInt(price * 100);
       console.log(amount, 'amount inside the intent')
@@ -220,28 +237,120 @@ async function run() {
     })
 
     // payment related api
-    app.get('/payments/:email', verifyToken, async(req,res) =>{
+    app.get('/payments/:email', verifyToken, async (req, res) => {
       const email = req.params.email;
-      const query = {email}
-      if(req.params.email !== req.decoded.email){
-        return res.status(403).send({ message: 'forbidden access'})
+      const query = { email }
+      if (req.params.email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
       }
       const result = await paymentCollection.find(query).toArray();
       res.send(result);
     })
 
-    app.post('/payments', async(req,res) =>{
+    app.post('/payments', async (req, res) => {
       const payment = req.body;
       const paymentResult = await paymentCollection.insertOne(payment);
 
       // carefully delete each item from the cart
       console.log('payment info', payment);
-      const query = {_id: {
-        $in: payment.cartIds.map(id => new ObjectId(id))
-      }};
+      const query = {
+        _id: {
+          $in: payment.cartIds.map(id => new ObjectId(id))
+        }
+      };
 
       const deleteResult = await cartCollection.deleteMany(query);
-      res.send({paymentResult, deleteResult})
+      res.send({ paymentResult, deleteResult })
+    })
+
+    // ssl payment
+    app.post('/create-ssl-payment', async (req, res) => {
+      const payment = req.body;
+      console.log('payment info', payment);
+      const trxId = new ObjectId().toString();
+      payment.transactionId = trxId;
+
+      const initiate = {
+        store_id: "bistr679e3defafc8c",
+        store_passwd: "bistr679e3defafc8c@ssl",
+        total_amount: payment.price,
+        currency: 'BDT',
+        tran_id: trxId, // use unique tran_id for each api call
+        success_url: 'http://localhost:5000/success-payment',
+        fail_url: 'http://localhost:5173/fail',
+        cancel_url: 'http://localhost:5173/cancel',
+        ipn_url: 'http://localhost:5001/ipn-success-payment',
+        shipping_method: 'Courier',
+        product_name: 'Computer.',
+        product_category: 'Electronic',
+        product_profile: 'general',
+        cus_name: 'Customer Name',
+        cus_email: `${payment.email}`,
+        cus_add1: 'Dhaka',
+        cus_add2: 'Dhaka',
+        cus_city: 'Dhaka',
+        cus_state: 'Dhaka',
+        cus_postcode: '1000',
+        cus_country: 'Bangladesh',
+        cus_phone: '01711111111',
+        cus_fax: '01711111111',
+        ship_name: 'Customer Name',
+        ship_add1: 'Dhaka',
+        ship_add2: 'Dhaka',
+        ship_city: 'Dhaka',
+        ship_state: 'Dhaka',
+        ship_postcode: 1000,
+        ship_country: 'Bangladesh',
+
+      }
+
+      const response = await axios({
+        url: "https://sandbox.sslcommerz.com/gwprocess/v4/api.php",
+        method: "POST",
+        data: initiate,
+        headers: {
+          "Content-Type": 'application/x-www-form-urlencoded'
+        }
+      })
+
+      const result = await paymentCollection.insertOne(payment);
+      const gatewayUrl = response?.data?.GatewayPageURL;
+      console.log('gatewayUrl', gatewayUrl);
+      res.send({gatewayUrl});
+    })
+
+    app.post('/success-payment', async(req,res) =>{
+      const paymentSuccess = req.body;
+      // console.log('Payment Success Info', paymentSuccess)
+      // validate
+      const {data} = await axios.get(`https://sandbox.sslcommerz.com/validator/api/validationserverAPI.php?val_id=${paymentSuccess.val_id}&store_id=bistr679e3defafc8c&
+       store_passwd=bistr679e3defafc8c@ssl&format=json`);
+       if(data.status !== 'VALID'){
+          return res.send({message: 'Invalid Payment'})
+       }
+
+       // update the payment
+       const updatePayment = await paymentCollection.updateOne(
+        {transactionId: data.tran_id},
+      {
+        $set:{
+          status:'success',
+        },
+      }
+    )
+    
+    const payment = await paymentCollection.findOne(
+      {transactionId: data.tran_id})
+
+      const query ={
+        _id:{
+          $in: payment.cartIds.map((id) => new ObjectId(id)),
+        },
+      }
+      const deleteResult = await cartCollection.deleteMany(query);
+      console.log('delete result', deleteResult);
+      res.redirect("http://localhost:5173/success")
+      // console.log(updatePayment, 'Update Payment')
     })
 
     // Send a ping to confirm a successful connection
@@ -255,11 +364,11 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/', (req,res) =>{
-    res.send('bistro boss is coming')
+app.get('/', (req, res) => {
+  res.send('bistro boss is coming')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
 
-    console.log(`Bistro Boss is coming on port: ${port}`);
+  console.log(`Bistro Boss is coming on port: ${port}`);
 })
